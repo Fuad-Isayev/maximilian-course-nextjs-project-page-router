f8b6bedbad0ad38941e4a5a44f06d1dd2673e1bb
Fuad-Isayev <fuad.isayev.1995@gmail.com>
2024-06-12 16:04:15 +0400

    revalidate meetup details page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -51,6 +51,7 @@ export async function getStaticProps(context) {
                 ...selectedMeetup,
                 _id: null,
             }
-        }
+        },
+        revalidate: 1
     }
 }
